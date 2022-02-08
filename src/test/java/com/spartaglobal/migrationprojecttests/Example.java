a60dd6d2ef4bb0cd6b6843c0938c9040cd1687a7
StephenJones1997 <98388181+StephenJones1997@users.noreply.github.com>
2022-02-08 16:12:17 +0000

    First attempts at tests

--- src/test/java/com/spartaglobal/migrationprojecttests/Example.java
+++ src/test/java/com/spartaglobal/migrationprojecttests/Example.java
@@ -1,4 +1,36 @@
 package com.spartaglobal.migrationprojecttests;
 
+import io.cucumber.messages.internal.com.fasterxml.jackson.annotation.ObjectIdGenerator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class Example {
+
+    @Test
+    @DisplayName("Check that the object is properly created based on the information")
+    void testObjectiscreatedProperly()
+    {
+        Object obj = new Object(1, "First Name", "Middle Name", "Last Name", "M", "Email", "DOB", "Doj", 10);
+
+        Assertions.assertEquals(obj.ID = 1, obj.namePrefix = "Mrs.", obj.FirstName = "First Name", obj.middleName = "Middle Name", obj.lastName = "lastName", obj.Gender = "M", obj.email = "Email", obj.DOB = "DOB" obj.DOJ = "DOJ", obj.Salary = 10);
+    }
+
+    @Test
+    @DisplayName("Check that the object is properly created based on the information")
+    void testObjectiscreatedProperly()
+    {
+        Object obj = new Object(1, "First Name", "Middle Name", "Last Name", "M", "Email", "DOB", "Doj", 10);
+
+        Assertions.assertEquals(obj.ID = 1, obj.namePrefix = "Mrs.", obj.FirstName = "First Name", obj.middleName = "Middle Name", obj.lastName = "lastName", obj.Gender = "M", obj.email = "Email", obj.DOB = "DOB" obj.DOJ = "DOJ", obj.Salary = 10);
+    }
+
+    @Test
+    @DisplayName("Check that the object is properly created with null values")
+    void testObjectiscreatedProperlywithNullValues()
+    {
+        Object obj = new Object(1, null, "Middle Name", "Last Name", "M", "Email", "DOB", "Doj", 10);
+
+        Assertions.assertEquals(obj.ID = 1, obj.namePrefix = null, obj.FirstName = "First Name", obj.middleName = "Middle Name", obj.lastName = "lastName", obj.Gender = "M", obj.email = "Email", obj.DOB = "DOB" obj.DOJ = "DOJ", obj.Salary = 10);
+    }
 }
