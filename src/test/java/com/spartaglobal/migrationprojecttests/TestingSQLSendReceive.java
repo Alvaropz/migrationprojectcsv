71e6824e7b89430fdb94df75de617e954ff97d48
brandonj987 <98341991+brandonj987@users.noreply.github.com>
2022-02-11 17:35:44 +0000

    Testing SQL send/receive

--- src/test/java/com/spartaglobal/migrationprojecttests/TestingSQLSendReceive.java
+++ src/test/java/com/spartaglobal/migrationprojecttests/TestingSQLSendReceive.java
@@ -1,103 +1,66 @@
 package com.spartaglobal.migrationprojecttests;
-
 import com.spartaglobal.database.DAOFactory;
 import com.spartaglobal.database.EmployeeDAO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 
 public class TestingSQLSendReceive {
 
     @Test
-    @DisplayName("Given an arrayList of positive integers, correctly send and receive the data from SQL")
-    public void givenAnArrayListOfPositiveIntegers_CorrectlySendAndReceiveBackToJava(){
-        ArrayList<String[]> testArray = new ArrayList<>();
-        String[]  expected = {"647173","Mr.","Milan","F","Krawczyk","M","milan.krawczyk@hotmail.com","4/4/1980","1/19/2012","123681"};
-        testArray.add(expected);
+    @DisplayName("Given an arrayList of data 1 row long, correctly send and receive the data from SQL")
+    public void givenAnArrayListOfDataOneRowLong_CorrectlySendAndReceiveBackToJava(){
+        // Creating array to be sent and received from SQL
+        ArrayList<String[]> a = new ArrayList<>();
+        String[]  row1a = {"1111111","Mr.","Bob","F","Smith","M","bob.smith@hotmail.com","1/1/2000","1/1/2000","1111111"};
+        a.add(row1a);
 
-
+        // Sending and receiving
         DAOFactory factoryType = DAOFactory.getDAOFactory();
         EmployeeDAO employeedao = factoryType.getEmployeeDAO();
         employeedao.createEmployeesTable();
-        employeedao.insertEmployee(testArray);
+        employeedao.insertEmployee(a);
+        ArrayList<String[]> actualArrList = employeedao.selectAllEmployees();
 
+        // Putting expected data into SQL format
+        ArrayList<String[]> expectedArrList = new ArrayList<>();
+        String[]  row1e = {"1111111","Mr.","Bob","F","Smith","M","bob.smith@hotmail.com","2000-01-01","2000-01-01","1111111.00"};
+        expectedArrList.add(row1e);
 
-        ArrayList<String[]> actual = employeedao.selectAllEmployees();
-//        StringBuffer actual = new StringBuffer();
-//        for (String[] employeeRow : retrievedData) {
-//            for (String employeeData : employeeRow) {
-//                actual.append(employeeData + " ");
-//            }
-//            actual.append("\n");
-//        }
-//
-//        StringBuffer expected = new StringBuffer();
-//        for (String[] employeeRow : retrievedData) {
-//            for (String employeeData : employeeRow) {
-//                actual.append(employeeData + " ");
-//            }
-//            actual.append("\n");
-//        }
-
-
-        System.out.println(testArray);
-        System.out.println(actual);
-        Assertions.assertArrayEquals(testArray.toArray(), actual.toArray());
-
-        // Pass into Alvaros method
-        // Class alvaro = new Class
-        // Something results = alvaro.method(testArray)
+        // Comparing arrays
+        var actual = actualArrList.toArray();
+        var expected = expectedArrList.toArray();
+        Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    @DisplayName("Given an arrayList of zeros, correctly send and receive the data from SQL")
-    public void givenAnArrayListOfZeros_CorrectlySendAndReceiveBackToJava(){
-        ArrayList<Object> testArray = new ArrayList<>();
-        testArray.add(0);
-        testArray.add(0);
-        testArray.add(0);
+    @DisplayName("Given an arrayList of data multiple rows long, correctly send and receive the data from SQL")
+    public void givenAnArrayListOfDataMultipleRowsLong_CorrectlySendAndReceiveBackToJava() {
+        // Creating array to be sent and received from SQL
+        ArrayList<String[]> a = new ArrayList<>();
+        String[] row1a = {"111111", "Mr.", "Bob", "F", "Smith", "M", "bob.smith@hotmail.com", "1/1/2000", "1/1/2000", "1111111"};
+        String[] row2a = {"222222", "Mr.", "Jim", "A", "Jones", "M", "jim.jones@hotmail.com", "2/2/2000", "2/2/2000", "2222222"};
+        a.add(row1a);
+        a.add(row2a);
 
-    }
+        // Sending and receiving
+        DAOFactory factoryType = DAOFactory.getDAOFactory();
+        EmployeeDAO employeedao = factoryType.getEmployeeDAO();
+        employeedao.createEmployeesTable();
+        employeedao.insertEmployee(a);
+        ArrayList<String[]> actualArrList = employeedao.selectAllEmployees();
 
-    @Test
-    @DisplayName("Given an arrayList of negative values, correctly send and receive the data from SQL")
-    public void givenAnArrayListOfNegativeValues_CorrectlySendAndReceiveBackToJava(){
-        ArrayList<Object> testArray = new ArrayList<>();
-        testArray.add(-1);
-        testArray.add(-2);
-        testArray.add(-3);
+        // Putting expected data into SQL format
+        ArrayList<String[]> expectedArrList = new ArrayList<>();
+        String[] row1e = {"111111", "Mr.", "Bob", "F", "Smith", "M", "bob.smith@hotmail.com", "2000-01-01", "2000-01-01", "1111111.00"};
+        String[] row2e = {"222222", "Mr.", "Jim", "A", "Jones", "M", "jim.jones@hotmail.com", "2000-02-02", "2000-02-02", "2222222.00"};
+        expectedArrList.add(row1e);
+        expectedArrList.add(row2e);
 
-    }
-
-    @Test
-    @DisplayName("Given an arrayList of strings, correctly send and receive the data from SQL")
-    public void givenAnArrayListOfStrings_CorrectlySendAndReceiveBackToJava(){
-        ArrayList<Object> testArray = new ArrayList<>();
-        testArray.add("test1");
-        testArray.add("test2");
-        testArray.add("test3");
-
-    }
-
-    @Test
-    @DisplayName("Given an arrayList of strings and integers, correctly send and receive the data from SQL")
-    public void givenAnArrayListOfStringsAndIntegers_CorrectlySendAndReceiveBackToJava(){
-        ArrayList<Object> testArray = new ArrayList<>();
-        testArray.add(1);
-        testArray.add("test2");
-        testArray.add(3);
-
-    }
-
-    @Test
-    @DisplayName("Given an arrayList of null, correctly send and receive the data from SQL")
-    public void givenAnArrayListOfNull_CorrectlySendAndReceiveBackToJava(){
-        ArrayList<Object> testArray = new ArrayList<>();
-        testArray.add(null);
-        testArray.add(null);
-        testArray.add(null);
-
+        // Comparing arrays
+        var actual = actualArrList.toArray();
+        var expected = expectedArrList.toArray();
+        Assertions.assertArrayEquals(expected, actual);
     }
 }
